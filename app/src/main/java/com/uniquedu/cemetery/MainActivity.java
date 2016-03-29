27bb868796bbd8bf28f70dfbb8c413cb392971b9
ZhongHang <zhonghangIT@163.com>
2016-03-29 16:00:36 +0800

    删除无用内容

--- app/src/main/java/com/uniquedu/cemetery/MainActivity.java
+++ app/src/main/java/com/uniquedu/cemetery/MainActivity.java
@@ -1,5 +1,6 @@
 package com.uniquedu.cemetery;
 
+import android.os.Build;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
