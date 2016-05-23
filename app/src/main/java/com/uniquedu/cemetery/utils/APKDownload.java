065c04c5b68cf555979969b27f064d142dba1f99
ZhongHang <zhonghangIT@163.com>
2016-05-23 13:39:29 +0800

    去掉了打包用的keystore

---
+++ app/src/main/java/com/uniquedu/cemetery/utils/APKDownload.java
@@ -0,0 +1,40 @@
+package com.uniquedu.cemetery.utils;
+
+import android.app.Activity;
+import android.app.DownloadManager;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+
+/**
+ * Created by ZhongHang on 2016/4/12.
+ */
+public class APKDownload {
+//    public static void download(Context context, String url) {
+//        Intent intent = new Intent();
+//        intent.setAction(Intent.ACTION_VIEW);
+//        intent.setData(Uri.parse(url));
+////        intent.setClassName("com.android.browser", "com.android.browser.BrowserActivity");
+//        context.startActivity(intent);
+//    }
+
+    public static void download(Context context, String url){
+        DownloadManager downloadManager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+
+        Uri uri = Uri.parse(url);
+        DownloadManager.Request request = new DownloadManager.Request(uri);
+
+        //设置允许使用的网络类型，这里是移动网络和wifi都可以
+        request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_MOBILE|DownloadManager.Request.NETWORK_WIFI);
+
+        //禁止发出通知，既后台下载，如果要使用这一句必须声明一个权限：android.permission.DOWNLOAD_WITHOUT_NOTIFICATION
+        //request.setShowRunningNotification(false);
+
+        //不显示下载界面
+//        request.setVisibleInDownloadsUi(false);
+        /*设置下载后文件存放的位置,如果sdcard不可用，那么设置这个将报错，因此最好不设置如果sdcard可用，下载后的文件        在/mnt/sdcard/Android/data/packageName/files目录下面，如果sdcard不可用,设置了下面这个将报错，不设置，下载后的文件在/cache这个  目录下面*/
+//request.setDestinationInExternalFilesDir(this, null, "tar.apk");
+        long id = downloadManager.enqueue(request);
+//TODO 把id保存好，在接收者里面要用，最好保存在Preferences里面
+    }
+}
